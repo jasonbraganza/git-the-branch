29c00d86146bbfb374c70cbdc664fc71a66099d3
Mario Jason Braganza <jason@janusworx.com>
2020-11-19 17:46:23 +0530

    still iterating
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

---
+++ gtb_legacy.py
@@ -0,0 +1,19 @@
+from git import Repo
+
+
+def main():
+    """
+    Uses GitPython to figure out local branches and print them out.
+    Has a blanket catch for any exceptions
+    """
+    try:
+        repo = Repo()
+        print("Local Branches in this repo are: ")
+        for each_item in repo.branches:
+            print(f"\t {each_item}")
+    except:
+        print("No git repo found in this folder")
+
+
+if __name__ == "__main__":
+    main()
